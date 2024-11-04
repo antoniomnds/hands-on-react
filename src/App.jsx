29a8eae58b3f2a500d3e89990c827b19dddadb8f
Antonio Santos <antoniomndsantos@gmail.com>
2024-11-04 22:45:53 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="container">
+      <article>
+        <hgroup>
+          <img src="/images/group.svg" alt="StarGazers Group"/>
+          <h1>Meet the StarGazers</h1>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They
+            are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+        </hgroup>
+      </article>
+    </div>
+  )
+}
+
+export default App
